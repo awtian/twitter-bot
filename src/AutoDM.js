44da9d784ced3e50b8988c0fab440bfdbb0b91b2
Awtian <me@awtian.com>
2019-08-23 00:45:29 +0700

    obsolte endpoints

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -1,34 +1,47 @@
 const T = require("./Twit.js");
 const my_user_name = require("../config").userName;
-const timeout = 1000 * 60 * 1; // timeout to send the message 5 min
+const timeout = 1000 * 60; // timeout to send the message 5 min
+
 
 const AutoDM = () => {
-  // const stream = T.stream("user");
+  console.log('hi iam here')
+//   // const stream = T.stream("user");
   setInterval(() => {
     console.log("Start Sending Auto Direct Message 🚀🚀🚀");
     SendMessage()
   }, timeout)
-  // stream.on("follow", SendMessage);
+//   // stream.on("follow", SendMessage);
 };
 
 const SendMessage = () => {
-  const screen_name = 'jeketifess'
+  // T.get('direct_messages/events/list', {count: 1})
+  //   .catch(e => console.log(e))
+  //   .then(result=>{
+  //     console.log(result.data)
+  //   })
+  // console.log('send message')
+  // const screen_name = 'jeketifess'
 
   const obj = {
-    screen_name,
-    text: 'i will flood your dm with shit instead. you delete the EG bashes or i\'ll just find more account to do this stupid shit'
+    type:'message_create',
+    message_create: {
+      target: {recipient_id: '1141197535110811648'},
+      message_data: {
+        text: 'i will flood your dm with shit instead. you delete the EG bashes or i\'ll just find more account to do this stupid shit'
+      }
+
+    }
   };
   // the follow stream track if I follow author person too.
-  if (screen_name != my_user_name) {
-    console.log(" time to spam ");
-      T.post("direct_messages/new", obj)
-        .catch(err => {
-          console.error("error", err.stack);
-        })
-        .then(result => {
-          console.log(`Message sent successfully To  ${screen_name}  💪💪`);
-        });
-  }
+  console.log(" time to spam ");
+    T.post("direct_messages/events/new", {event: obj})
+      .catch(err => {
+        console.error("error", err.stack);
+      })
+      .then(result => {
+        console.log((result.data))
+        console.log(`Message sent successfully To  💪💪`);
+      });
 };
 
 module.exports = AutoDM;
